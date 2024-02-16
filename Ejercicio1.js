220c10880fa5b327ff595b104130b84f1ad22514
Stefa2707 <153329504+Stefa2707@users.noreply.github.com>
2024-02-15 20:53:19 -0500

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,8 @@
+// Crear un array con al menos 5 nombres de personas
+let nombres = ["Nataly", "Valeria", "Valentina", "Angie", "Stefa"];
+
+// Recorrer el array e imprimir cada nombre en la consola
+console.log("Nombres en el array:");
+
+// Imprimir la longitud del array
+console.log("Longitud del array: " + nombres.length);
